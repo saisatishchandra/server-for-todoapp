b9ebfc22e546f7f5d409c30e93f0da4be6abdf78
saisatishchandra <satishmallavarapu111.com>
2025-10-18 09:14:03 +0530

    for render

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-import jsonServer from "json-server";
+const path = require("path");
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
